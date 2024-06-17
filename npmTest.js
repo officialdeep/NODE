42e76e33a4e714a58f3d22473dcc2f4bc4191bcc
deepak <officialdeep.y@gmail.com>
2024-06-17 20:03:20 +0530

    first commit

---
+++ npmTest.js
@@ -0,0 +1,10 @@
+const figlet = require('./node_modules/figlet')
+
+figlet("Deepak Kumar", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
